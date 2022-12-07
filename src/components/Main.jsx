50d25e0d3e204a6b7f8d53cd293b483b6c7df215
SiriFazendoBarra <manuel.martinez.perezdearce@gmail.com>
2022-12-06 21:08:08 -0300

    7th commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import Product from './Product'
 
 function Main (){
         return(
-        <main className='main d-grid p-4 gap-4' style={{gridArea:"main", gridTemplateColumns:"repeat(auto-fill, minmax(min(100%, 15rem), 1fr))", background:"#381446" }}>
+        <main className='main container d-grid p-4 gap-4' style={{gridArea:"main", gridTemplateColumns:"repeat(auto-fill, minmax(min(100%, 15rem), 1fr))", background:"#381446" }}>
             
             <Product title="Rick Sanchez" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" text="Human"/>
             <Product title="Morty Smith" src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" text="Human"/>
